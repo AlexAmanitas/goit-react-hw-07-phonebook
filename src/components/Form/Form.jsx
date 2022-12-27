96f2d87785331fab304e635142fb0e38e981af75
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2022-12-27 12:00:58 +0200

    start

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,53 @@
+import uniqid from 'uniqid';
+import { TheForm, Label, Input, SubmitBtn } from './Form.styled';
+import { useDispatch } from 'react-redux';
+import { addContacts } from 'redux/sliceContacts';
+
+const Form = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    const form = evt.target;
+    dispatch(
+      addContacts({
+        id: uniqid(),
+        name: form.elements.name.value,
+        number: form.elements.number.value,
+      })
+    );
+    form.reset();
+  };
+
+  return (
+    <TheForm action="" onSubmit={handleSubmit}>
+      <Label htmlFor="name">Name</Label>
+      <Input
+        id="name"
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        // value={name}
+        // onChange={handleChange}
+      />
+
+      <Label htmlFor="tel">Number</Label>
+      <Input
+        id="tel"
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        // value={number}
+        // onChange={handleChange}
+      />
+
+      <SubmitBtn type="submit">Add contact</SubmitBtn>
+    </TheForm>
+  );
+};
+
+export default Form;
