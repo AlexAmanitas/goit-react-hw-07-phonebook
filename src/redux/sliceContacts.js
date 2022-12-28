28d7ffb1b60cf8cf8df21a26b7d9382d72018811
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2022-12-28 18:46:46 +0200

    Update sliceContacts.js

--- src/redux/sliceContacts.js
+++ src/redux/sliceContacts.js
@@ -6,17 +6,6 @@ import {
   filterContacts,
 } from './operations';
 
-// const testContacts = [
-//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//   { id: 'id-5', name: 'Edem Cldfmts', number: '645-17-79' },
-//   { id: 'id-6', name: 'Alec Mjduels', number: '645-17-79' },
-//   { id: 'id-7', name: 'Karl Fridr', number: '645-17-79' },
-//   { id: 'id-8', name: 'Joiur Masuro', number: '645-17-79' },
-// ];
-
 const handlePending = state => {
   state.isLoading = true;
 };
